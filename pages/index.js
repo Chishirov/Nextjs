11e1d6e730614cdc1969dcd8301491718a5d238c
Dimitar Chishirov <dimitarchishirov@gmail.com>
2024-04-24 17:36:12 +0200

    up description

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Head from "next/head"
 const heroProps = {
   image: { src: "/img/heroimg.png", alt: "", width: 1000, height: 1680 },
   title: "Agency procurement, outsourced.",
-  description: "Start the process here",
+  description: "Start the process here fdslkföldskföldsköldfsk",
   ctaText: "Start",
 };
 
